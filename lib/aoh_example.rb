29ddb1726504fa98595699a01b7cb59b1b431c77
Garret Gallant <gallanttechnological@gmail.com>
2020-09-02 06:31:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,8 @@ def literal_aoh
   # held in the constants
   
   literal_array_of_hashes = 
-  [{:name => "Don Gately", :occupation => "Live-in staff"}
+  [{:name => "Don Gately", 
+  :occupation => "Live-in staff"},
    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
    {:name => "Pat Monteseian", :occupation => "Staff"},
    {:name => "Kate Gompert", :occupation => "None"},
