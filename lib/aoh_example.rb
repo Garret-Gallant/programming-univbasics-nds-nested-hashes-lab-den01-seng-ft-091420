63db6172d3cd7ec291c703c899a599abfc432f32
Garret Gallant <gallanttechnological@gmail.com>
2020-09-02 06:32:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,11 +19,15 @@ def literal_aoh
   
   literal_array_of_hashes = 
   [{:name => "Don Gately", 
-  :occupation => "Live-in staff"},
-   {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
-   {:name => "Pat Monteseian", :occupation => "Staff"},
-   {:name => "Kate Gompert", :occupation => "None"},
-   {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
+    :occupation => "Live-in Staff"},
+   {:name => "Joelle van Dyne", 
+    :occupation => "Radio Personality"},
+   {:name => "Pat Monteseian", 
+    :occupation => "Staff"},
+   {:name => "Kate Gompert", 
+    :occupation => "None"},
+   {:name => "Bruce Green", 
+    :occupation => "Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
